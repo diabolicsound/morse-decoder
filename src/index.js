70d8c5f744f90400adb4d488443b1c609df8527f
Artsemi Shareika <artsemishareika@gmail.com>
2020-03-10 14:56:13 +0300

    complete task morse-decoder

--- src/index.js
+++ src/index.js
@@ -38,8 +38,45 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+   let sortedarr = expr.toString().match(/.{1,2}/g);
+   let newarr = []
+   for (let i = 0; i < sortedarr.length; i++) {
+       if (sortedarr[i] == '00') {
+           sortedarr[i] = '~~'
+    newarr.push (sortedarr[i]);
+       } 
+        else if (sortedarr[i] == '10') {
+           sortedarr[i] = '.~';
+        newarr.push (sortedarr[i]);
+       }
+       else if (sortedarr[i] == '11') {
+           sortedarr[i] = '-~';
+        newarr.push (sortedarr[i]);
+       }
+       else if (sortedarr[i] == '**') {
+        newarr.push (sortedarr[i]);
+       } 
+    }
+    let newarrjoined = newarr.join('');
+    let resarr = [];
+
+    newarrjoined = newarrjoined.toString().match(/.{1,10}/g);
+     for (let j = 0; j < newarrjoined.length; j++) {
+        resarr.push(newarrjoined[j].replace(/~/g, ''));
+ }
+let finalarr = [];
+
+ for (let t = 0; t < resarr.length; t++) {
+     if (resarr[t] == '**********') {
+     finalarr.push(' ')
+     }
+     else {
+finalarr.push(MORSE_TABLE[resarr[t]]);
+ }
 }
+return finalarr.join('');
+}
+
 
 module.exports = {
     decode
